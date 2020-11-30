e2090153251f484d1f58bf3761c5a3e74d446763
ypq <374563453@qq.com>
2020-11-30 18:11:25 +0800

    appointment提前一天发短信通知用户

--- appointmentList/appointmentList.pb.micro.go
+++ appointmentList/appointmentList.pb.micro.go
@@ -5,7 +5,6 @@ package appointmentList
 
 import (
 	fmt "fmt"
-	_ "github.com/cargod-bj/b2c-proto-common/common"
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/ptypes/any"
 	math "math"
@@ -35,32 +34,32 @@ var _ context.Context
 var _ client.Option
 var _ server.Option
 
-// Api Endpoints for Demo service
+// Api Endpoints for AppointmentList service
 
-func NewDemoEndpoints() []*api.Endpoint {
+func NewAppointmentListEndpoints() []*api.Endpoint {
 	return []*api.Endpoint{}
 }
 
-// Client API for Demo service
+// Client API for AppointmentList service
 
-type DemoService interface {
+type AppointmentListService interface {
 	GetListForSMS(ctx context.Context, in *SmsCond, opts ...client.CallOption) (*Response, error)
 }
 
-type demoService struct {
+type appointmentListService struct {
 	c    client.Client
 	name string
 }
 
-func NewDemoService(name string, c client.Client) DemoService {
-	return &demoService{
+func NewAppointmentListService(name string, c client.Client) AppointmentListService {
+	return &appointmentListService{
 		c:    c,
 		name: name,
 	}
 }
 
-func (c *demoService) GetListForSMS(ctx context.Context, in *SmsCond, opts ...client.CallOption) (*Response, error) {
-	req := c.c.NewRequest(c.name, "Demo.GetListForSMS", in)
+func (c *appointmentListService) GetListForSMS(ctx context.Context, in *SmsCond, opts ...client.CallOption) (*Response, error) {
+	req := c.c.NewRequest(c.name, "AppointmentList.GetListForSMS", in)
 	out := new(Response)
 	err := c.c.Call(ctx, req, out, opts...)
 	if err != nil {
@@ -69,27 +68,27 @@ func (c *demoService) GetListForSMS(ctx context.Context, in *SmsCond, opts ...cl
 	return out, nil
 }
 
-// Server API for Demo service
+// Server API for AppointmentList service
 
-type DemoHandler interface {
+type AppointmentListHandler interface {
 	GetListForSMS(context.Context, *SmsCond, *Response) error
 }
 
-func RegisterDemoHandler(s server.Server, hdlr DemoHandler, opts ...server.HandlerOption) error {
-	type demo interface {
+func RegisterAppointmentListHandler(s server.Server, hdlr AppointmentListHandler, opts ...server.HandlerOption) error {
+	type appointmentList interface {
 		GetListForSMS(ctx context.Context, in *SmsCond, out *Response) error
 	}
-	type Demo struct {
-		demo
+	type AppointmentList struct {
+		appointmentList
 	}
-	h := &demoHandler{hdlr}
-	return s.Handle(s.NewHandler(&Demo{h}, opts...))
+	h := &appointmentListHandler{hdlr}
+	return s.Handle(s.NewHandler(&AppointmentList{h}, opts...))
 }
 
-type demoHandler struct {
-	DemoHandler
+type appointmentListHandler struct {
+	AppointmentListHandler
 }
 
-func (h *demoHandler) GetListForSMS(ctx context.Context, in *SmsCond, out *Response) error {
-	return h.DemoHandler.GetListForSMS(ctx, in, out)
+func (h *appointmentListHandler) GetListForSMS(ctx context.Context, in *SmsCond, out *Response) error {
+	return h.AppointmentListHandler.GetListForSMS(ctx, in, out)
 }
